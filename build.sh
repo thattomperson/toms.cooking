4fab5cd7e2eeeb1143a60dd312ee0170aa1e72a3
Thomas Albrighton <thomasalbrighton@gmail.com>
2019-03-26 11:18:38 +1030

    now

---
+++ build.sh
@@ -0,0 +1,8 @@
+yum install -y wget
+
+HUGO_VERSION=0.54.0
+
+wget https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
+tar -xzf hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
+
+./hugo
