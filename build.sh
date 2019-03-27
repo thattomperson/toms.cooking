dd50f8316a4345f2a39114094a07db45e901c225
Thomas Albrighton <thomasalbrighton@gmail.com>
2019-03-27 12:53:42 +1030

    minify

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ HUGO_VERSION=0.54.0
 wget https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
 tar -xzf hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
 
-./hugo
+./hugo --minify
