63daa7b6b25630d1f34b7e2ec7800060834f6df1
Thomas Albrighton <thomasalbrighton@gmail.com>
2019-09-24 18:17:58 +0930

    test

---
+++ src/components/Card.svelte
@@ -0,0 +1,34 @@
+<div class="card">
+  <img class="card-image" src="{img}" alt="{name}">
+  <div class="card-content">
+    <h1 class="card-heading">{name}</h1>
+  </div>
+</div>
+
+
+<style>
+  .card {
+    border-radius: 8px;
+    width: 295px;
+    overflow: hidden;
+    background-color: white; 
+  }
+
+  .card-content {
+
+  }
+
+  .card-image {
+    height: 130px;
+    width: 100%;
+
+    object-fit: cover;
+  }
+</style>
+
+
+<script>
+  export let img = ""
+  export let name = ""
+
+</script>
---
+++ src/routes/styleguide.svelte
@@ -0,0 +1,14 @@
+
+
+<script>
+  import Card from '../components/Card.svelte';
+  import Grid from '../components/Grid.svelte';
+
+
+</script>
+
+
+
+<Grid>
+  <Card img="/pasta.jpg" name="Zucchini, Garlic, Chilli, & Lemon Spaghetti With Parmesan"></Card>
+</Grid>
